4862328d132b83291395aaae0ebee4609c4d8700
Elizaveta Ladygina <dranyinosok@gmail.com>
2022-06-12 20:04:26 +0300

    first task completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,21 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  
+  arr = [];
+  let disc = b**2 - 4 * a * c;
+
+  if (disc < 0) {
+    arr;
+  } else if (disc === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(disc)) / (2 * a))
+    arr.push((-b - Math.sqrt(disc)) / (2 * a))
+    console.log(arr)
+  }
+
   return arr; // array
 }
 
@@ -8,6 +23,7 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
+  // not done yet
 
   return totalAmount;
 }
