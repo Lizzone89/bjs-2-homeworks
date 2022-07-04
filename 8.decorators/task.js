ac8095392fc4a74fca54af44b36e45a75682f62d
Elizaveta Ladygina <dranyinosok@gmail.com>
2022-07-04 07:58:29 +0300

    task completed

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,59 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+function wrapper(...args) {
+  const hash = args.join(',')
+  let index = cache.findIndex((item)=> item.hash === hash )
+  if (index !== -1 ) { 
+      console.log("Из кэша: " + cache[index].value)
+      return "Из кэша: " + cache[index].value;
+    }
+
+  let result = func(...args);
+  cache.push({hash: hash, value: result});
+   if (cache.length > 5) { 
+    cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+}
+
+return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, interval) {
+
+  let timeout;
+
+  function wrapper(args) {
+    if (!timeout) {
+      func.apply(this, args);
+     }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, interval);
+   }
+
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, interval) {
+  
+  let timeout;
+  wrapper.count = 0;
+
+  function wrapper(args) {
+    if (!timeout) {
+      func.apply(this, args);
+      wrapper.count++;
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+    func.apply(this, args);
+    wrapper.count++;
+    }, interval);
+  }
+  return wrapper;
 }
